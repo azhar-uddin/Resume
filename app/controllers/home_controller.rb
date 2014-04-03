0b54a5c23fe04e0630bd9c4325514fd7f1729b04
re11 <re11@re11-desktop.(none)>
2014-04-03 14:58:27 +0530

    My Resume

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+
+  def home
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def education
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def projects
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def skills
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def personal
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AzharWeb::Application.config.secret_key_base = 'd733c1bc0bb44adce11ff347f0e1046f5a27d51c5b44cf058abd790f2af825b49587289e1eb2ad9f8da5e95dd0488928cbf7490bc536f68f9accab28c0a1e0f6'
